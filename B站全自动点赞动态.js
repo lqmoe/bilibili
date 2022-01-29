7b0b7a1882c49268dac5b009fed4e0ee6546023d
lqmoe <98631254+lqmoe@users.noreply.github.com>
2022-01-29 12:13:27 +0800

    Add files via upload

---
+++ B站全自动点赞动态.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         B站全自动点赞动态
+// @version      0.1
+// @author       绫浅
+// @match        https://t.bilibili.com/*
+// @run-at       document-end
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+/* global $ */
+
+dianzan();
+function dianzan() {
+    $(".custom-like-icon.zan").filter(
+        function() {
+            return( this.className.split(/\s+/).length == 2 );
+        }
+    ).first().trigger('click');
+    setTimeout(dianzan, 1000);//点赞间隔时间，以毫秒为单位
+}
+
+shuaxin();
+function shuaxin() {
+    let time=30000;//刷新界面间隔时间，以毫秒为单位
+    setTimeout(() => {
+        location.reload()
+    },time);
+}
